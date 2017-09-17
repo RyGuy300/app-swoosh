9ca3f2c5fd2c3e415f36cdd6b0aa455e0471772c
Ryland Arnold <ryland@rarnold.com>
2017-09-17 14:11:15 -0400

    Segues and unwind

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -26,6 +26,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
 
 
 }
