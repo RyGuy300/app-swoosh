6e39f6a17b9a73d6e5645e210d1fd2e64494fd7a
Ryland Arnold <ryland@rarnold.com>
2017-09-17 14:48:02 -0400

    Added VC's, and programed from league to skill VC

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,36 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by Ryland Arnold on 9/17/17.
+//  Copyright © 2017 Ryland Arnold. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
