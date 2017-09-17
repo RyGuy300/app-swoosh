473904f132ae518b7f4f801f6912c4ed2fa85503
Ryland Arnold <ryland@rarnold.com>
2017-09-17 15:29:15 -0400

    Completed App

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Ryland Arnold on 9/17/17.
+//  Copyright © 2017 Ryland Arnold. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
